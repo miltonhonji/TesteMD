ee7e7b93c7921a587e9e65c453c2b1a0cb99bcf2
Milton Honji <factorysolutionIT@outlook.com>
2024-11-09 23:01:08 -0300

    Implementado as funcionalidades para o Produtos

---
+++ DeMaria.BLL/ProdutoRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeMaria.BLL.Contracts;
+using DeMaria.DTO;
+using DeMaria.DAL;
+using Npgsql;
+using System.Data;
+
+namespace DeMaria.BLL
+{
+    class ProdutoRepository : IBaseRepository<Produto>, IDisposable
+    {
+        public int Deletar(Produto TEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(Produto TEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Produto> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto Obter(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Produto TEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeMaria.DTO/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeMaria.DTO
+{
+    public class Produto : Base
+    {
+        public string Descricao { get; set; }
+        public Decimal Preco { get; set; }
+        public int Estoque { get ; set; }
+    }
+}
